a60fcf8001cfa7ac233b91a89f10c5dc87bf4010
Mahesh Chavda <chavdamahesh2772@gmail.com>
2025-12-13 22:58:04 +0530

    Database fetch as api and server component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 import { ComponentExample } from "@/components/component-example";
+import { prisma } from "@/lib/prisma";
 
-export default function Page() {
-return <ComponentExample />;
+export default async function Page() {
+    const users = await prisma.user.findMany();
+    return (
+        <>
+            <h1>Users</h1>
+            <hr />
+            <ul>
+                {users.map((user, idx: number) => (
+                    <li key={idx}>Mahesh, {user.name}</li>
+                ))}
+            </ul>
+        </>
+    );
 }
--- app/test/page.tsx
+++ app/test/page.tsx
@@ -6,6 +6,9 @@ export default async function Page() {
         <div className="p-6">
             <h1 className="text-xl font-bold">Mahesh Chavda</h1>
             <p className="mt-4 text-green-600">{data.message}</p>
+            {data?.data?.map(user => (
+                <h4 key={user.id}>{user.name}</h4>
+            ))}
         </div>
     );
 }
