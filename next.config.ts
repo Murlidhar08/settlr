b1a405b0601c7ff44dac4341f5d53520c819542e
Murlidhar Properties <Murlidharproperties08@gmail.com>
2025-12-13 16:25:07 +0530

    Initial project files

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,7 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from "next";
 
-import initializeBundleAnalyzer from '@next/bundle-analyzer';
-
-// https://www.npmjs.com/package/@next/bundle-analyzer
-const withBundleAnalyzer = initializeBundleAnalyzer({
-    enabled: process.env.BUNDLE_ANALYZER_ENABLED === 'true'
-});
-
-// https://nextjs.org/docs/pages/api-reference/next-config-js
 const nextConfig: NextConfig = {
-    output: 'standalone'
+  /* config options here */
 };
 
-export default withBundleAnalyzer(nextConfig);
+export default nextConfig;
