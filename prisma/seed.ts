da462563f4d1ffd2dc2cf1022b41d8438ce9272c
Mahesh Chavda <chavdamahesh2772@gmail.com>
2025-12-18 09:13:02 +0530

    Seed updat

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,26 @@ import { prisma } from "@/lib/prisma"
 
 // Default Data
 async function main() {
-    console.log("SEEDING COMPLETED...")
+    const devUser = await prisma.user.create({
+        data: {
+            email: 'dev@settlr.com',
+            name: 'Dev User',
+            password: "randomPassword",
+            contactNo: "9998887776",
+            address: "Sample Address, Dev Enviroment, Settlr",
+            profileUrl: "https://i.pravatar.cc/300"
+        },
+    });
+
+    const business = await prisma.business.create({
+        data: {
+            name: "Dev Business",
+            ownerId: 1,
+        }
+    })
+
+
+    console.log({ business, devUser });
 }
 
 // Execution
