81d361735dae1f6f5e97bda026c4721b4344a943
Murlidhar Properties <Murlidharproperties08@gmail.com>
2026-02-24 19:49:56 +0530

    build version design improvement

--- components/auth/build-version.tsx
+++ components/auth/build-version.tsx
@@ -5,8 +5,8 @@ export async function BuildVersion() {
 
     return (
         <div className="fixed bottom-4 right-4 z-50 opacity-40 hover:opacity-100 transition-opacity">
-            <p className="text-[10px] font-black uppercase tracking-[0.3em]">
-                Build Version {version}
+            <p className="text-[10px] font-bold tracking-[0.3em] text-white">
+                Build v{version}
             </p>
         </div>
     );
