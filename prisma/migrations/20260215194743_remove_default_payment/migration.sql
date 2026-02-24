42798ae808b36c58e53186d67cea9bf9cff0390a
Murlidhar Properties <Murlidharproperties08@gmail.com>
2026-02-24 13:20:57 +0530

    delete all migration

--- prisma/migrations/20260215194743_remove_default_payment/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `defaultPayment` on the `userSettings` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "userSettings" DROP COLUMN "defaultPayment";
--- prisma/migrations/20260224074802_account_to_account/migration.sql
+++ prisma/migrations/20260224074802_account_to_account/migration.sql
@@ -1,17 +1,20 @@
 -- CreateEnum
-CREATE TYPE "PaymentMode" AS ENUM ('CASH', 'ONLINE');
-
--- CreateEnum
 CREATE TYPE "Currency" AS ENUM ('INR', 'USD', 'EUR');
-
--- CreateEnum
-CREATE TYPE "TransactionDirection" AS ENUM ('IN', 'OUT');
 
 -- CreateEnum
 CREATE TYPE "ThemeMode" AS ENUM ('AUTO', 'LIGHT', 'DARK');
 
 -- CreateEnum
-CREATE TYPE "PartyType" AS ENUM ('CUSTOMER', 'SUPPLIER', 'BOTH');
+CREATE TYPE "FinancialAccountType" AS ENUM ('MONEY', 'PARTY', 'CATEGORY');
+
+-- CreateEnum
+CREATE TYPE "MoneyType" AS ENUM ('CASH', 'ONLINE', 'CHEQUE');
+
+-- CreateEnum
+CREATE TYPE "PartyType" AS ENUM ('CUSTOMER', 'SUPPLIER', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "CategoryType" AS ENUM ('INCOME', 'EXPENSE', 'ASSET', 'EQUITY', 'ADJUSTMENT');
 
 -- CreateTable
 CREATE TABLE "user" (
@@ -24,7 +27,12 @@ CREATE TABLE "user" (
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "contactNo" TEXT,
     "address" TEXT,
+    "activeBusinessId" TEXT,
     "twoFactorEnabled" BOOLEAN DEFAULT false,
+    "role" TEXT,
+    "banned" BOOLEAN DEFAULT false,
+    "banReason" TEXT,
+    "banExpires" TIMESTAMP(3),
 
     CONSTRAINT "user_pkey" PRIMARY KEY ("id")
 );
@@ -39,7 +47,7 @@ CREATE TABLE "session" (
     "ipAddress" TEXT,
     "userAgent" TEXT,
     "userId" TEXT NOT NULL,
-    "activeBusinessId" TEXT,
+    "impersonatedBy" TEXT,
 
     CONSTRAINT "session_pkey" PRIMARY KEY ("id")
 );
@@ -89,8 +97,9 @@ CREATE TABLE "twoFactor" (
 CREATE TABLE "userSettings" (
     "userId" TEXT NOT NULL,
     "currency" "Currency" NOT NULL DEFAULT 'INR',
-    "dateFormat" TEXT NOT NULL DEFAULT 'DD/MM/YYYY',
-    "defaultPayment" "PaymentMode" NOT NULL DEFAULT 'CASH',
+    "dateFormat" TEXT NOT NULL DEFAULT 'dd/MM/yyyy',
+    "timeFormat" TEXT NOT NULL DEFAULT 'hh:mm a',
+    "language" TEXT NOT NULL DEFAULT 'en',
     "theme" "ThemeMode" NOT NULL DEFAULT 'AUTO',
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -109,13 +118,31 @@ CREATE TABLE "business" (
 );
 
 -- CreateTable
+CREATE TABLE "financialAccount" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "businessId" TEXT NOT NULL,
+    "type" "FinancialAccountType" NOT NULL,
+    "moneyType" "MoneyType",
+    "partyType" "PartyType",
+    "categoryType" "CategoryType",
+    "partyId" TEXT,
+    "isSystem" BOOLEAN NOT NULL DEFAULT false,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "financialAccount_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "party" (
     "id" TEXT NOT NULL,
-    "businessId" TEXT NOT NULL,
     "name" TEXT NOT NULL,
     "contactNo" TEXT,
-    "type" "PartyType" NOT NULL,
     "profileUrl" TEXT,
+    "businessId" TEXT NOT NULL,
+    "isActive" BOOLEAN NOT NULL DEFAULT true,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -127,28 +154,16 @@ CREATE TABLE "transaction" (
     "id" TEXT NOT NULL,
     "businessId" TEXT NOT NULL,
     "amount" DECIMAL(12,2) NOT NULL,
-    "mode" "PaymentMode" NOT NULL,
-    "direction" "TransactionDirection" NOT NULL,
     "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "description" TEXT,
     "userId" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "fromAccountId" TEXT NOT NULL,
+    "toAccountId" TEXT NOT NULL,
     "partyId" TEXT,
 
     CONSTRAINT "transaction_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
-CREATE TABLE "transactionDocument" (
-    "id" TEXT NOT NULL,
-    "name" TEXT NOT NULL,
-    "extension" TEXT NOT NULL,
-    "transactionId" TEXT NOT NULL,
-    "userId" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT "transactionDocument_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
@@ -173,19 +188,46 @@ CREATE INDEX "twoFactor_secret_idx" ON "twoFactor"("secret");
 CREATE INDEX "twoFactor_userId_idx" ON "twoFactor"("userId");
 
 -- CreateIndex
+CREATE INDEX "business_ownerId_idx" ON "business"("ownerId");
+
+-- CreateIndex
+CREATE INDEX "financialAccount_businessId_idx" ON "financialAccount"("businessId");
+
+-- CreateIndex
+CREATE INDEX "financialAccount_partyId_idx" ON "financialAccount"("partyId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "financialAccount_businessId_name_key" ON "financialAccount"("businessId", "name");
+
+-- CreateIndex
 CREATE INDEX "party_businessId_idx" ON "party"("businessId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "party_businessId_name_key" ON "party"("businessId", "name");
 
 -- CreateIndex
 CREATE INDEX "transaction_businessId_idx" ON "transaction"("businessId");
 
 -- CreateIndex
-CREATE INDEX "transaction_date_idx" ON "transaction"("date");
+CREATE INDEX "transaction_businessId_date_idx" ON "transaction"("businessId", "date");
 
 -- CreateIndex
-CREATE INDEX "transaction_businessId_mode_idx" ON "transaction"("businessId", "mode");
+CREATE INDEX "transaction_businessId_fromAccountId_idx" ON "transaction"("businessId", "fromAccountId");
 
 -- CreateIndex
-CREATE INDEX "transactionDocument_transactionId_idx" ON "transactionDocument"("transactionId");
+CREATE INDEX "transaction_businessId_toAccountId_idx" ON "transaction"("businessId", "toAccountId");
+
+-- CreateIndex
+CREATE INDEX "transaction_fromAccountId_date_idx" ON "transaction"("fromAccountId", "date");
+
+-- CreateIndex
+CREATE INDEX "transaction_toAccountId_date_idx" ON "transaction"("toAccountId", "date");
+
+-- CreateIndex
+CREATE INDEX "transaction_userId_idx" ON "transaction"("userId");
+
+-- CreateIndex
+CREATE INDEX "transaction_partyId_date_idx" ON "transaction"("partyId", "date");
 
 -- AddForeignKey
 ALTER TABLE "session" ADD CONSTRAINT "session_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
@@ -203,6 +245,12 @@ ALTER TABLE "userSettings" ADD CONSTRAINT "userSettings_userId_fkey" FOREIGN KEY
 ALTER TABLE "business" ADD CONSTRAINT "business_ownerId_fkey" FOREIGN KEY ("ownerId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "financialAccount" ADD CONSTRAINT "financialAccount_businessId_fkey" FOREIGN KEY ("businessId") REFERENCES "business"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "financialAccount" ADD CONSTRAINT "financialAccount_partyId_fkey" FOREIGN KEY ("partyId") REFERENCES "party"("id") ON DELETE SET NULL ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "party" ADD CONSTRAINT "party_businessId_fkey" FOREIGN KEY ("businessId") REFERENCES "business"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
@@ -212,10 +260,10 @@ ALTER TABLE "transaction" ADD CONSTRAINT "transaction_businessId_fkey" FOREIGN K
 ALTER TABLE "transaction" ADD CONSTRAINT "transaction_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
+ALTER TABLE "transaction" ADD CONSTRAINT "transaction_fromAccountId_fkey" FOREIGN KEY ("fromAccountId") REFERENCES "financialAccount"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "transaction" ADD CONSTRAINT "transaction_toAccountId_fkey" FOREIGN KEY ("toAccountId") REFERENCES "financialAccount"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "transaction" ADD CONSTRAINT "transaction_partyId_fkey" FOREIGN KEY ("partyId") REFERENCES "party"("id") ON DELETE SET NULL ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "transactionDocument" ADD CONSTRAINT "transactionDocument_transactionId_fkey" FOREIGN KEY ("transactionId") REFERENCES "transaction"("id") ON DELETE CASCADE ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "transactionDocument" ADD CONSTRAINT "transactionDocument_userId_fkey" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
