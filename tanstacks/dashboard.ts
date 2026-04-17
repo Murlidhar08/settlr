6cbe30dd1f091c0aa53463d0be326453a2eaeb0c
Murlidhar Properties <Murlidharproperties08@gmail.com>
2026-04-17 14:15:43 +0530

    Dashboard cache

--- tanstacks/dashboard.ts
+++ tanstacks/dashboard.ts
@@ -1,7 +1,11 @@
 import { getBudgetInsights } from "@/actions/transaction.actions";
+import { useSession } from "@/lib/auth/auth-client";
 import { useQuery } from "@tanstack/react-query";
 
-export const useBudgetInsights = (businessId?: string | null) => {
+export const useBudgetInsights = () => {
+    const { data: session } = useSession();
+    const businessId = session?.user?.activeBusinessId;
+
     return useQuery({
         queryKey: ["budget-insights", businessId],
         queryFn: () => getBudgetInsights(),
